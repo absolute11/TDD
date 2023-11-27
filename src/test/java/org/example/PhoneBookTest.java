db5054cd61985770f646761adb441a06722dc5e7
absolute11 <den.gitelman@gmail.com>
2023-11-27 21:13:05 +0300

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
+    @Test
+    public void testAddContact(){
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("John Smit", "123456789"));
+        assertEquals(2, phoneBook.add("Jane Smit", "987654321"));
+    }
 
 
 }
