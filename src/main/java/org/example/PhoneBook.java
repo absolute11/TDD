f17d84a4d7f068234bc195880fc12764e6a5d920
absolute11 <den.gitelman@gmail.com>
2023-11-27 21:19:19 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,8 +1,15 @@
 package org.example;
 
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
     public int add(String name, String number){
+        if(!contacts.containsKey(name)){
+            contacts.put(name,number);
+            return contacts.size();
+        }
         return 0;
     }
 
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PhoneBookTest {
     @Test
-    public void testAddContact(){
+    public void testAddContact() {
         PhoneBook phoneBook = new PhoneBook();
         assertEquals(1, phoneBook.add("John Smit", "123456789"));
         assertEquals(2, phoneBook.add("Jane Smit", "987654321"));
     }
-
-
 }
